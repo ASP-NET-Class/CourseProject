013869f9d32d6ceae10d90543d1bec28f1de54e6
Jdsapp <jdsapp337@gmail.com>
2020-10-17 14:11:14 -0500

    Update SwimmerController.cs

--- Controllers/SwimmerController.cs
+++ Controllers/SwimmerController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using CourseProject.Models;
 using Microsoft.AspNetCore.Authorization;
@@ -10,7 +12,8 @@ namespace CourseProject.Controllers
 {
     // makes it so only Swimmers and Administrators have access to the Swimmer Controller 
     // and being able to make changes
-    [Authorize(Roles= "Swimmer, Administrator")]
+    [Authorize(Roles = "Swimmer, Administrator")]
+
     public class SwimmerController : Controller
     {
         private readonly ApplicationDbContext db;
@@ -22,6 +25,44 @@ namespace CourseProject.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+         public IActionResult AddProfile()
+        {
+            var currentUserId = this.User.FindFirst
+                (ClaimTypes.NameIdentifier).Value;
+            Swimmer swimmer = new Swimmer();
+            if (db.Swimmers.Any(i => i.UserId ==
+                currentUserId))
+                {
+                swimmer = db.Swimmers.FirstOrDefault(i =>
+                i.UserId == currentUserId);
+                }
+            else
+            {
+                swimmer.UserId = currentUserId;
+            }
+            return View(swimmer);
+            
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddProfile
+            (Swimmer swimmer)
+        {
+            var currentUserId = this.User.FindFirst
+                (ClaimTypes.NameIdentifier).Value;
+            if(db.Swimmers.Any(i => i.UserId == currentUserId))
+            {
+                var swimmerToUpdate = db.Swimmers.FirstOrDefault
+                    (i => i.UserId == currentUserId);
+                swimmerToUpdate.SwimmerName = swimmer.SwimmerName;
+                db.Update(swimmerToUpdate);
+            }
+            else
+            {
+                db.Add(swimmer);
+            }
+            await db.SaveChangesAsync();
+            return View("Index");
         }
     }
 }
