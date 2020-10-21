6d063be78c3b8fb613a0cab41ecd865829a18e04
robert gzyl <robertlgzyl@gmail.com>
2020-10-21 08:20:54 -0700

    Added a Migration, joining.

---
+++ Migrations/20201021030533_M60.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CourseProject.Migrations
+{
+    public partial class M60 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Admins");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DailyStartTime",
+                table: "Sessions",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EndDate",
+                table: "Sessions",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartDate",
+                table: "Sessions",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DailyStartTime",
+                table: "Sessions");
+
+            migrationBuilder.DropColumn(
+                name: "EndDate",
+                table: "Sessions");
+
+            migrationBuilder.DropColumn(
+                name: "StartDate",
+                table: "Sessions");
+
+            migrationBuilder.CreateTable(
+                name: "Admins",
+                columns: table => new
+                {
+                    AdminId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AdminEmail = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AdminName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Admins", x => x.AdminId);
+                });
+        }
+    }
+}
